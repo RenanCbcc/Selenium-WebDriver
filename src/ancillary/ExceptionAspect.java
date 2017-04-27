d68dfdafbe6baa58b8a0fc50b16c1d285abb8396
Renan Thiago <renannojosa@gmail.com>
2017-04-27 11:22:19 -0300

    Update ExceptionAspect.java
    Todas as Exceções são agora tratadas em uma única classe(Aspecto).

--- src/ancillary/ExceptionAspect.java
+++ src/ancillary/ExceptionAspect.java
@@ -17,8 +17,36 @@ public class ExceptionAspect {
 	 @Pointcut("execution(* consultar_requisicao_pagamento.Teste_Consulta.*(..))")
 	 private void NSEexception(){}
 	
-	
-		
+	 @AfterThrowing(
+		      pointcut = "execution(* alterar_requisicao_pagamento_beneficiario.Teste_Altera_Beneficiario.*(..))",
+		      throwing= "error")
+		    public void afterThrowing_Teste_Altera_Beneficiario(JoinPoint joinPoint, Throwable error) {
+		 	logger.info("logAfterThrowing() is running!");
+		 	logger.info("Local : " + joinPoint.getSignature().getName());
+		 	logger.log(Level.SEVERE, error.getMessage(), error);
+		    }
+
+
+	 @AfterThrowing(
+		      pointcut = "execution(* alterar_requisicao_pagamento_dados_processo.Teste_Altera_Dados_Processo.*(..))",
+		      throwing= "error")
+		    public void afterThrowing_Teste_Altera_Dados_Processo(JoinPoint joinPoint, Throwable error) {
+		 	logger.info("logAfterThrowing() is running!");
+		 	logger.info("Local : " + joinPoint.getSignature().getName());
+		 	logger.log(Level.SEVERE, error.getMessage(), error);
+		    }
+
+	 
+	 @AfterThrowing(
+		      pointcut = "execution(* alterar_requisicao_pagamento_terceiro_interessado.Altera_Terceiros_Interessados.*(..))",
+		      throwing= "error")
+		    public void afterThrowing_Altera_Terceiros_Interessados(JoinPoint joinPoint, Throwable error) {
+		 	logger.info("logAfterThrowing() is running!");
+		 	logger.info("Local : " + joinPoint.getSignature().getName());
+		 	logger.log(Level.SEVERE, error.getMessage(), error);
+		    }
+
+
 	 @AfterThrowing(
 		      pointcut = "execution(* consultar_requisicao_pagamento.Teste_Consulta.*(..))",
 		      throwing= "error")
@@ -38,15 +66,6 @@ public class ExceptionAspect {
 		    }
 
 	 @AfterThrowing(
-		      pointcut = "execution(* alterar_requisicao_pagamento_dados_processo.Teste_Altera_Dados_Processo.*(..))",
-		      throwing= "error")
-		    public void afterThrowing_Teste_Altera_Dados_Processo(JoinPoint joinPoint, Throwable error) {
-		 	logger.info("logAfterThrowing() is running!");
-		 	logger.info("Local : " + joinPoint.getSignature().getName());
-		 	logger.log(Level.SEVERE, error.getMessage(), error);
-		    }
-
-	 @AfterThrowing(
 		      pointcut = "execution(* incluir_requisicao_pagamento_beneficiario.Teste_Inclui_Beneficiario.*(..))",
 		      throwing= "error")
 		    public void afterThrowing_Teste_Inclui_Beneficiario(JoinPoint joinPoint, Throwable error) {
@@ -54,10 +73,33 @@ public class ExceptionAspect {
 		 	logger.info("Local : " + joinPoint.getSignature().getName());
 		 	logger.log(Level.SEVERE, error.getMessage(), error);
 		    }
-
 	 
+	 @AfterThrowing(
+		      pointcut = "execution(* incluir_requisicao_pagamento_terceiro_interessado.Teste_Inclui_Terceiros_Interessados.*(..))",
+		      throwing= "error")
+		    public void afterThrowing_Teste_Inclui_Terceiros_Interessados(JoinPoint joinPoint, Throwable error) {
+		 	logger.info("logAfterThrowing() is running!");
+		 	logger.info("Local : " + joinPoint.getSignature().getName());
+		 	logger.log(Level.SEVERE, error.getMessage(), error);
+		    }
+	 
+	 @AfterThrowing(
+		      pointcut = "execution(* visualizar_detalhes_historicos_requisicao_pagamento.Teste_Visualiza_Detalhes.*(..))",
+		      throwing= "error")
+		    public void afterThrowing_Teste_Visualiza_Detalhes(JoinPoint joinPoint, Throwable error) {
+		 	logger.info("logAfterThrowing() is running!");
+		 	logger.info("Local : " + joinPoint.getSignature().getName());
+		 	logger.log(Level.SEVERE, error.getMessage(), error);
+		    }
 
-	
+	 @AfterThrowing(
+		      pointcut = "execution(* visualizar_Informacoes_resumidas_requisicao_pagamento.Teste_Visualiza.*(..))",
+		      throwing= "error")
+		    public void afterThrowing_Teste_Visualiza(JoinPoint joinPoint, Throwable error) {
+		 	logger.info("logAfterThrowing() is running!");
+		 	logger.info("Local : " + joinPoint.getSignature().getName());
+		 	logger.log(Level.SEVERE, error.getMessage(), error);
+		    }
+ 
 
-
-	}
+}
